208b14d746b427e4af7365c2c30146d652ea09a0
Aditya <adityaprakash.bobby@gmail.com>
2019-03-25 22:40:10 +0530

    Approach one

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flaskext.markdown import Markdown
+from flask_babel import _
 
 app = Flask(__name__)
 
@@ -7,14 +8,23 @@ markdown_app = Markdown(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    # if request.method == "POST":
-    mkd = open('test.md', 'r')
+    if request.method == "POST":
+        # mkd = open('test.md', 'r')
 
-    mkd = mkd.read()
+        # mkd = mkd.read()
+
+        mkd = request.form['simplemarkdown']
+        
+        return render_template('home.html', mkd=mkd)
+    
+    mkd = 'None'
 
     return render_template('home.html', mkd=mkd)
 
-    # return render_template('home.html', mkd='None')
+@app.route('/markdown', methods=['POST'])
+def markdown():
+
+    return jsonify({'mkd':request.form['simplemarkdown']})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='8000')
