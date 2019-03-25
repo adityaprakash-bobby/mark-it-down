e9254e374ad2ca3790c94f4eb0fb60bd700e374f
Aditya <adityaprakash.bobby@gmail.com>
2019-03-26 00:18:14 +0530

    Added support for emojis <3 and checklist

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
 from flask import Flask, render_template, request, jsonify
-from flaskext.markdown import Markdown
+# from flaskext.markdown import Markdown
 from flask_babel import _
+
+import markdown
+from markdown import Markdown
+from markdown_checklist.extension import ChecklistExtension
+from mdx_unimoji import UnimojiExtension
 
 app = Flask(__name__)
 
-markdown_app = Markdown(app)
+# markdown_app = Markdown(app)
+
+md = Markdown(extensions=[ChecklistExtension(), UnimojiExtension()])
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == "POST":
-        # mkd = open('test.md', 'r')
 
-        # mkd = mkd.read()
+        mkd = md.convert(request.form['simplemarkdown'])
 
-        mkd = request.form['simplemarkdown']
-        
         return render_template('home.html', mkd=mkd)
     
     mkd = ''
